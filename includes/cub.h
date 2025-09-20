d2bcfead8723de4cb344e7a95b9df6fbf2be7fd6
Fenohasina Samy Andriatafitantsoa <fsamy-an@student.42antananarivo.mg>
2025-09-20 10:31:01 +0300

    maKEFILE

--- includes/cub.h
+++ includes/cub.h
@@ -6,11 +6,16 @@
 /*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 08:58:59 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/09/17 09:17:52 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/09/20 10:30:52 by fsamy-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_H
 #define CUB_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
 
 #endif
