bf6c46fe647ab13da73357ede2a74721d2bcc1a1
Fenohasina Samy Andriatafitantsoa <fsamy-an@student.42antananarivo.mg>
2025-09-20 11:42:18 +0300

    bregin

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:17:47 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/03/11 16:58:25 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/09/20 11:40:51 by fsamy-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ void	ft_putstr_fd(char *str, int fd)
 	int	i;
 
 	i = 0;
+	if (!str)
+	{
+		return ;
+	}
 	while (str[i])
 	{
 		write(fd, &str[i], 1);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 10:22:35 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/09/20 11:25:23 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/09/20 11:38:42 by fsamy-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,35 @@ read in the file
 use gnl
 */
 
+void	display_map(int fd)
+{
+	char	*str;
 
+	//str = NULL;
+	while (str)
+	{
+		str = get_next_line(fd);
+		ft_putstr_fd(str ,1);
+		free(str);
+	}
+}
 int	main(int argc, char **argv)
 {
 	(void)argv;
+	int fd;
+	
 	if (argc != 2)
 	{
 		ft_putstr_fd("Error\nUsage: ./cub3D <path to the map>\n",2);
+		return (0);
 	}
+	fd = open (argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putstr_fd("Error\nNo such file or directory\n",2);
+		return (0);
+	}
+	display_map(fd);
 
 
 
