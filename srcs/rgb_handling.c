00a3256fab51761536a05ead329ab455cb7f598c
mratsima <mratsima@student.42antananarivo.mg>
2025-10-02 22:50:39 +0300

    Initialized uninit value

--- srcs/rgb_handling.c
+++ srcs/rgb_handling.c
@@ -6,7 +6,7 @@
 /*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:57:44 by mratsima          #+#    #+#             */
-/*   Updated: 2025/09/25 17:12:19 by mratsima         ###   ########.fr       */
+/*   Updated: 2025/10/02 22:45:25 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ int	get_rgb(t_rgb *rgb, char *og_rgb)
 	char	*str_rgb;
 	int	i;
 	
+	i = 0;
 	while (og_rgb[i])
 	{
 		if (og_rgb[i] == ',' && og_rgb[i + 1] == ',')
@@ -72,6 +73,7 @@ int	get_rgb(t_rgb *rgb, char *og_rgb)
 	rgb->green = atoll(split_rgb[1]);
 	rgb->blue = atoll(split_rgb[2]);
 	free(str_rgb);
+	free_split(split_rgb);
 	// printf("rgb = %d, %d, %d\n", rgb->red, rgb->green, rgb->blue);
 	return (0);
 }
