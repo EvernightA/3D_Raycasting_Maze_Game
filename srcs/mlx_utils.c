43e5e3d38ba4fe4e213629d79fc8bf8468f6653b
mratsima <mratsima@student.42antananarivo.mg>
2025-10-20 10:11:36 +0300

    inside map

--- srcs/mlx_utils.c
+++ srcs/mlx_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mlx_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
+/*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 11:50:26 by mratsima          #+#    #+#             */
-/*   Updated: 2025/10/20 09:40:06 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/10/20 10:11:16 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,47 +31,51 @@ int key_hook(int key, void *param)
 		quit_win(display);
 	else if (key == XK_Up)
 	{
-		//if (display->map[display->player.y_blocs - 1][display->player.x_blocs] == '0')
-		//{
+		if (display->map[display->player.y_blocs - 1][display->player.x_blocs] == '0')
+		{
 			display->player.y_pixel--;
-			//display->map[display->player.y_blocs + 1][display->player.x_blocs] = '0';
-			//display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
+			display->player.y_blocs = display->player.y_pixel / 16;
+			display->map[display->player.y_blocs + 1][display->player.x_blocs] = '0';
+			display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
 			print_map(display->map);
 			puts("~\n~");
-		//}
+		}
 	}
 	else if (key == XK_Down)
 	{
-		//if (display->map[display->player.y_blocs + 1][display->player.x_blocs] == '0')
-		//{
+		if (display->map[display->player.y_blocs + 1][display->player.x_blocs] == '0')
+		{
 			display->player.y_pixel++;
-			//display->map[display->player.y_blocs - 1][display->player.x_blocs] = '0';
-			//display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
+			display->player.y_blocs = display->player.y_pixel / 16;
+			display->map[display->player.y_blocs - 1][display->player.x_blocs] = '0';
+			display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
 			print_map(display->map);
 			puts("~\n~");
-		//}
+		}
 	}
 	else if (key == XK_Left)
 	{
-		//if (display->map[display->player.y_blocs][display->player.x_blocs - 1] == '0')
-		//{	
+		if (display->map[display->player.y_blocs][display->player.x_blocs - 1] == '0')
+		{	
 			display->player.x_pixel--;
-			//display->map[display->player.y_blocs][display->player.x_blocs + 1] = '0';
-			//display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
+			display->player.x_blocs = display->player.x_pixel / 16;
+			display->map[display->player.y_blocs][display->player.x_blocs + 1] = '0';
+			display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
 			print_map(display->map);
 			puts("~\n~");
-		//}
+		}
 	}
 	else if (key == XK_Right)
 	{
-		//if (display->map[display->player.y_blocs][display->player.x_blocs + 1] == '0')
-		//{
+		if (display->map[display->player.y_blocs][display->player.x_blocs + 1] == '0')
+		{
 			display->player.x_pixel++;
-			//display->map[display->player.y_blocs][display->player.x_blocs - 1] = '0';
-			//display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
+			display->player.x_blocs = display->player.x_pixel / 16;
+			display->map[display->player.y_blocs][display->player.x_blocs - 1] = '0';
+			display->map[display->player.y_blocs][display->player.x_blocs] = display->player.orientation;
 			print_map(display->map);
 			puts("~\n~");
-		//}
+		}
 	}
 	mlx_clear_window(display->mlx.mlx_ptr, display->mlx.win_ptr);
 	mini_map(display, display->map);
