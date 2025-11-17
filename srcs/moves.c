f1d33712b9bcb8a73df1b28b3a94f518d7699bea
mratsima <mratsima@student.42antananarivo.mg>
2025-11-17 10:22:17 +0300

    optimized render

--- srcs/moves.c
+++ srcs/moves.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
+/*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 17:41:59 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/11/14 12:09:41 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/11/17 10:15:55 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,9 @@ int game_engine(t_display *display)
 			{
 				player_move (display, 1, 1, display->player.angle);
 			}
-		// render_all(display);
+		clear_img(display);
+    	clear_rays(display);
+		render_all(display);
 	}
 	if (display->key_stat.s_press)
 	{
@@ -89,8 +91,9 @@ int game_engine(t_display *display)
 		{
 			player_move (display, -1, -1, display->player.angle);
 		}
-		// render_all(display);
-    
+		clear_img(display);
+    	clear_rays(display);
+		render_all(display);   
 	}
 	if (display->key_stat.a_press)
 		// tmp is too see if next is a wall
@@ -109,7 +112,9 @@ int game_engine(t_display *display)
 			display->player.blocs = pixel_to_bloc(display->player.pixels, display);
 			display->map[display->player.blocs.y][display->player.blocs.x] = display->player.orientation;
 		}
-		// render_all(display);
+		clear_img(display);
+    	clear_rays(display);
+		render_all(display);
 	}
 	if (display->key_stat.d_press)
 	{
@@ -129,7 +134,9 @@ int game_engine(t_display *display)
 			display->player.blocs = pixel_to_bloc(display->player.pixels, display);
 			display->map[display->player.blocs.y][display->player.blocs.x] = display->player.orientation;
 		}
-		// render_all(display);
+		clear_img(display);
+    	clear_rays(display);
+		render_all(display);
 	}
 	if (display->key_stat.left_press)
 	{
@@ -137,7 +144,9 @@ int game_engine(t_display *display)
 		orientation_player(display, -1);
 		rad_to_deg(display->player.angle);
 		// rad_to_deg(display->player.rl_angle);
-		// render_all(display);
+		clear_img(display);
+    	clear_rays(display);
+		render_all(display);
 	}
 	if (display->key_stat.right_press)
 	{
@@ -145,8 +154,10 @@ int game_engine(t_display *display)
 		orientation_player(display, 1);
 		rad_to_deg(display->player.angle);
 		// rad_to_deg(display->player.angle);
+		clear_img(display);
+   		clear_rays(display);
+		render_all(display);
 	}
-	render_all(display);
     // ft_putstr_fd("WUUUUUT\n", 1);
         // sleep(1);
     return (0);
