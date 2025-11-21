6847bbedcebb6f03bd4183547ea77ff2e72d3775
Fenohasina Samy Andriatafitantsoa <fsamy-an@student.42antananarivo.mg>
2025-11-21 16:28:20 +0300

    clear_win

--- srcs/moves.c
+++ srcs/moves.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
+/*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 17:41:59 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/11/20 10:55:45 by mratsima         ###   ########.fr       */
+/*   Updated: 2025/11/21 16:26:32 by fsamy-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void	clean_rendering(t_display *display)
 {
-	clear_img(display);
-   	clear_rays(display);
+	//clear_img(display);
+   	//clear_rays(display);
+	mlx_clear_window(display->mlx.mlx_ptr, display->mlx.win_ptr);
 	render_all(display);
 }
 
@@ -89,5 +90,7 @@ int game_engine(t_display *display)
 		orientation_player(display, 1);
 		clean_rendering(display);
 	}
+		//clean_rendering(display);
+
     return (0);
 }
