c7731caf0ac7df52b7ee443cb0ffe22cdcf0bb81
mratsima <mratsima@student.42antananarivo.mg>
2025-11-29 09:59:37 +0300

    norme1

--- srcs/dda.c
+++ srcs/dda.c
@@ -6,54 +6,61 @@
 /*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 09:26:52 by mratsima          #+#    #+#             */
-/*   Updated: 2025/11/29 09:32:22 by mratsima         ###   ########.fr       */
+/*   Updated: 2025/11/29 09:58:27 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub.h"
 
+int	calculate_steps_and_increment(t_dda *dda, t_point *begin)
+{
+	if (fabsf(dda->dx) > fabsf(dda->dy))
+		dda->steps = fabsf(dda->dx);
+	else
+		dda->steps = fabsf(dda->dy);
+	if (dda->steps == 0)
+	{
+		dda->new_node = ft_linenew(*begin);
+		return (0);
+	}
+	dda->x_inc = dda->dx / dda->steps;
+	dda->y_inc = dda->dy / dda->steps;
+	return (1);
+}
+
+void	update_current(t_dda *dda)
+{
+	dda->current.f_x = dda->x;
+	dda->current.f_y = dda->y;
+	dda->current.x = (int)dda->x;
+	dda->current.y = (int)dda->y;
+	dda->current.dp = 0;
+}
+
+int initialize(t_dda *dda, t_point *begin, t_point *end)
+{
+	dda->head = NULL;
+	dda->tail = NULL;
+	// Use the precise coordinates for calculation
+	dda->dx = end->f_x - begin->f_x;
+	dda->dy = end->f_y - begin->f_y;
+	if (!calculate_steps_and_increment(dda, begin))
+		return (0);
+	dda->x = begin->f_x;
+	dda->y = begin->f_y;
+	dda->i = 0;
+	return (1);
+}
+
 t_line	*dda_line(t_point *begin, t_point *end)
 {
 	t_dda dda;
-	
-	dda.head = NULL;
-	dda.tail = NULL;
-	
-	// Use the precise coordinates for calculation
-	dda.dx = end->f_x - begin->f_x;
-	dda.dy = end->f_y - begin->f_y;
-	
-	// Calculate dda.steps - take the larger absolute value
-	if (fabsf(dda.dx) > fabsf(dda.dy))
-		dda.steps = fabsf(dda.dx);
-	else
-		dda.steps = fabsf(dda.dy);
-	
-	// Avoid division by zero
-	if (dda.steps == 0)
-	{
-		dda.new_node = ft_linenew(*begin);
-		return (dda.new_node);
-	}
-	
-	// Calculate increment for each step
-	dda.x_inc = dda.dx / dda.steps;
-	dda.y_inc = dda.dy / dda.steps;
-	
-	// Start at beginning point
-	dda.x = begin->f_x;
-	dda.y = begin->f_y;
-	
-	dda.i = 0;
+
+	if (!initialize(&dda, begin, end))
+		return (0);
 	while (dda.i <= (int)dda.steps)
 	{
-		// Store both integer and float versions
-		dda.current.f_x = dda.x;
-		dda.current.f_y = dda.y;
-		dda.current.x = (int)dda.x;  // Just truncate - no rounding!
-		dda.current.y = (int)dda.y;  // Just truncate - no rounding!
-		dda.current.dp = 0;
-		
+		update_current(&dda);
 		dda.new_node = ft_linenew(dda.current);
 		if (dda.head == NULL)
 		{
@@ -65,11 +72,9 @@ t_line	*dda_line(t_point *begin, t_point *end)
 			dda.tail->next = dda.new_node;
 			dda.tail = dda.new_node;
 		}
-		
 		dda.x += dda.x_inc;
 		dda.y += dda.y_inc;
 		dda.i++;
 	}
-	
 	return (dda.head);
 }
