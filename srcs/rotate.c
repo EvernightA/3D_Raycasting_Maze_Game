aa29d7d7405f592f1ff81a26b4549265824c75d9
mratsima <mratsima@student.42antananarivo.mg>
2025-12-01 08:05:11 +0300

    norme5

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
+/*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 09:08:08 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/11/25 13:21:36 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/12/01 08:04:53 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,31 +30,8 @@ void	rotate_player(t_display *display, float angle)
 	tmpy = 0.0;
 	tmpx = (display->end.x - display->begin.x) * cos(angle) - (display->end.y - display->begin.y) * sin(angle) + display->begin.x;
 	tmpy = (display->end.y - display->begin.y) * cos(angle) + (display->end.x - display->begin.x) * sin(angle) + display->begin.y;
-	printf("rx = %f, ry= %f\n", tmpx, tmpy);
 	display->end.x = fabs(tmpx);
 	display->end.y = fabs(tmpy);
 	display->end.f_x = fabs(tmpx);
 	display->end.f_y = fabs(tmpy);
 }
-
-
-//void	rotate_player(t_display *display, float angle)
-//{
-//	float tmpx;
-//	float tmpy;
-//	float dx;
-//	float dy;
-
-//	// Calculate the direction vector from player to laser endpoint
-//	dx = display->end.x - display->begin.x;
-//	dy = display->end.y - display->begin.y;
-	
-//	// Apply the correct 2D rotation formula
-//	tmpx = dx * cos(angle) - dy * sin(angle) + display->begin.x;
-//	tmpy = dx * sin(angle) + dy * cos(angle) + display->begin.y;
-
-//	display->end.x = tmpx;
-//	display->end.y = tmpy;
-//	printf("end x = %d ; ", display->end.x);
-//	printf("end y = %d\n\n", display->end.y);
-//}
