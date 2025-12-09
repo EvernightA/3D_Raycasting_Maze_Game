43f5eb04e189d7afe5db9a7bb29860b2495ca328
mratsima <mratsima@student.42antananarivo.mg>
2025-12-09 18:43:12 +0300

    norme3

--- srcs/line_drawing.c
+++ srcs/line_drawing.c
@@ -6,7 +6,7 @@
 /*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 10:34:32 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/12/09 18:38:19 by mratsima         ###   ########.fr       */
+/*   Updated: 2025/12/09 18:42:51 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	init_hit(t_hit *hit)
 {
-	(void)hit;
 	hit->distance = 0;
 	hit->collision.x = 0;
 	hit->collision.y = 0;
 	hit->collision.f_x = 0;
 	hit->collision.f_y = 0;
-	// before = NULL;
 }
 
 void	wall_assign(t_hit *hit, t_line *tmp, t_display *display, float beta)
@@ -64,11 +62,7 @@ t_hit	draw_line_2(t_display *display, float beta)
 	while (tmp)
 	{
 		tmp_bloc = pixel_to_bloc(tmp->dot, display);
-		if (is_walkable(display, tmp_bloc))
-		{
-			// img_pix_put(&display->rays, tmp->dot.x, tmp->dot.y, 0x00F0);
-		}
-		else
+		if (!is_walkable(display, tmp_bloc))
 		{
 			wall_assign(&hit, tmp, display, beta);
 			direction_fix(display, &hit, bloc);
