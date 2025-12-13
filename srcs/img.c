2fd87bfe6508f861b0492712396c332cbb1fda68
mratsima <mratsima@student.42antananarivo.mg>
2025-12-13 14:31:14 +0300

    clean4

--- srcs/img.c
+++ srcs/img.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   img.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
+/*   By: mratsima <mratsima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 16:41:21 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/12/09 08:57:17 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/12/13 14:30:47 by mratsima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,41 +24,5 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 		}
 		pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
 		*(int *)pixel = color;
-	}
-}
-
-void	clear_img(t_display *display)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < SCRN_HEIGHT)
-	{
-		j = 0;
-		while (j < SCRN_WIDTH)
-		{
-			img_pix_put(&display->all, j, i, 0x00000);
-			j++;
-		}
-		i++;
-	}
-}
-
-void	clear_rays(t_display *display)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < SCRN_HEIGHT)
-	{
-		j = 0;
-		while (j < SCRN_WIDTH)
-		{
-			img_pix_put(&display->rays, j, i, 0x00000);
-			j++;
-		}
-		i++;
 	}
 }
