45f496870969b4d956a3d9a2a1180479aee748c8
Fenohasina Samy Andriatafitantsoa <fsamy-an@student.42antananarivo.mg>
2025-12-15 15:03:00 +0300

    noseg

--- srcs/moves.c
+++ srcs/moves.c
@@ -6,7 +6,7 @@
 /*   By: fsamy-an <fsamy-an@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 17:41:59 by fsamy-an          #+#    #+#             */
-/*   Updated: 2025/12/09 13:40:36 by fsamy-an         ###   ########.fr       */
+/*   Updated: 2025/12/15 15:02:10 by fsamy-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int	there_is_no_wall(t_display *display, int op, bool is_float)
 		tmp = pixel_to_bloc(tmp, display);
 	}
 	if (tmp.x < display->texture.map_width && display->map[tmp.y][tmp.x]
-		&& display->map[tmp.y][tmp.x] != '1')
+		&& display->map[tmp.y][tmp.x] != '1' && display->texture.dup_map[tmp.y][tmp.x] != 'X' && display->texture.dup_map[tmp.y][tmp.x] != ' ')
 	{
 		return (1);
 	}
